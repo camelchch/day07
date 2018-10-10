7e0eded84717b9a3cd6b83fe6417a3a4c21e6d8d
Saiqiong XIAO <saxiao@e1r12p10.42.fr>
2018-10-10 20:43:48 +0200

    update

---
+++ newtest/ex01/Euron.class.php
@@ -0,0 +1,11 @@
+<?php
+
+include("/Users/saxiao/day07/ex01/Greyjoy.class.php");
+
+class Euron extends Greyjoy {
+	public function announceMotto() {
+		print($this->familyMotto . PHP_EOL);
+	}
+}
+
+?>
